d0c4b4e8ae966701b6f092f5aa7a1d53f84ece8c
bruno081016-ship-it <bruno081016@gmail.com>
2025-11-11 13:46:45 -0300

    Calculador simples de prestação
    Calculadora feita em Python, utilizando while, if, elif e else.

--- Calculo_de_prestação.py
+++ Calculo_de_prestação.py
@@ -1,12 +1,18 @@
-casa=float(input("Valor da casa : "))
-salario=float(input("Seu salario :  "))
-anos= int(input("Quantos anos de financiamento ?  "))
-prestracao = casa/(anos*12)
-minimo=salario * 30/100
-print(f"Para pagar uma casa de RS{casa: .2f} em {anos} anos")
-print(f"A prestação sera de R${prestracao: .2f}")
-if prestracao <= minimo:
-    print("Emprestimo pode ser aprovado. ")
-else:
-    print("Emprestimo negado. ")
-print(input("Deseja fazer uma nova consulta ? Digite S/N: "))
+import os
+resp = "S" or "s"
+while resp in ["S","s"]:
+    os.system("cls")
+    casa=float(input("Valor da casa : "))
+    salario=float(input("Seu salario :  "))
+    anos= int(input("Quantos anos de financiamento ?  "))
+    prestracao = casa/(anos*12)
+    minimo=salario * 30/100
+    print(f"Para pagar uma casa de RS{casa: .2f} em {anos} anos")
+    print(f"A prestação sera de R${prestracao: .2f}")
+    if prestracao <= minimo:
+        print("Emprestimo pode ser aprovado. ")
+    else:
+        print("Emprestimo negado. ")
+    resp = input("Deseja fazer uma nova consulta ? Digite S/N: ") 
+os.system("cls")
+print("Fim do programa. ")
